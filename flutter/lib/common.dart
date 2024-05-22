b3301de0ec390200e9caaf3ef9710fa26025378f
Jemmota <129885668+Jemmota@users.noreply.github.com>
2024-05-22 09:26:45 -0300

    Update common.dart

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -3160,7 +3160,7 @@ Widget loadPowered(BuildContext context) {
     cursor: SystemMouseCursors.click,
     child: GestureDetector(
       onTap: () {
-        launchUrl(Uri.parse('https://rustdesk.com'));
+        launchUrl(Uri.parse('https://softjem.com.br'));
       },
       child: Opacity(
           opacity: 0.5,
